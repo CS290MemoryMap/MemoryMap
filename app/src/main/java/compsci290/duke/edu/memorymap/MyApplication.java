98589a1584c287d0b989e99ee1fca70672a46fe2
Saeed Alrahma <sas.al.rahma@gmail.com>
2017-04-12 12:54:03 -0400

    Added MyApplication class to access context.

---
+++ app/src/main/java/compsci290/duke/edu/memorymap/MyApplication.java
@@ -0,0 +1,21 @@
+package compsci290.duke.edu.memorymap;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Application class used to access context
+ */
+
+public class MyApplication extends Application {
+    private static Context context;
+
+    public void onCreate() {
+        super.onCreate();
+        MyApplication.context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return MyApplication.context;
+    }
+}
